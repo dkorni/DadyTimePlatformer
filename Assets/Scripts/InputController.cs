e9adf4294e66ebee061b87de2525ebf822e25e5c
Denys Korniienko <den.creativ.kor@gmail.com>
2020-12-12 19:25:03 +0200

    Scene

---
+++ Assets/Scripts/InputController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputController : MonoBehaviour
+{
+
+    public InputController Instance
+    {
+        get
+        {
+            if (_instance == null)
+                _instance = FindObjectOfType<InputController>();
+
+            return _instance;
+        }
+    }
+
+    private InputController _instance;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
